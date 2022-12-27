f39fb7f1003b14bbfa18d6251fb187b898a1326a
AKSHAY0243 <120465437+AKSHAY0243@users.noreply.github.com>
2022-12-27 18:32:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log("Hello and welcome to bootcamp");
+console.log("this is fifth day");
+
+// var, let, const
+var price = 30;
+console.log(price);
+price = "Aman";
+console.log(price);
+
+// conditional statements in js: if-else if-else
+
+let name = "ajit";
+console.log(name);
+name = 30.8;
+//let name = "Aman"
+console.log(name);
+
+const pi = 3.14;
+console.log(pi);
+
+// let age = 11;
+// if (age < 18) {
+//     console.log("not eligible");
+// }    else if (age >= 18 && age <= 60) {
+//     console.log("eligible");
+// } else {
+// console.log("age limit exceeded");
+// }  
+
+function printHelloNTimes(n) {
+      for (let i = 1; i <= n; i++) {
+     console.log("Hello: " + i);
+   }
+}
